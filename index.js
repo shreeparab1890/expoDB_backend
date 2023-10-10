28c7ccd0acecd61a55224acfe47c2b8e587f1237
Shrikrishna Parab <40030115+shreeparab1890@users.noreply.github.com>
2023-10-10 13:33:35 +0530

    Update index.js

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const port = process.env.PORT || 5001;
 app.use(function (req, res, next) {
 
     // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', 'https://places.hinterscapes.com');
+    res.setHeader('Access-Control-Allow-Origin', 'https://famous-sunflower-ae081d.netlify.app');
 
     // Request methods you wish to allow
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
