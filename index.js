0d29d317b7f4820e29a230ddeb68499e47715632
Shrikrishna Parab <40030115+shreeparab1890@users.noreply.github.com>
2023-10-10 13:32:49 +0530

    Update index.js

--- index.js
+++ index.js
@@ -11,6 +11,26 @@ app.use(express.json());
 app.use(cors());
 const port = process.env.PORT || 5001;
 
+// Add headers before the routes are defined
+app.use(function (req, res, next) {
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', 'https://places.hinterscapes.com');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+});
+
 //Test API: GET /api/v1/
 app.get("/api/v1/", (req, res) => {
   const ip = req.headers["x-forwarded-for"] || req.connection.remoteAddress;
